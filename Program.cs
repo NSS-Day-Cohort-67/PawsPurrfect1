83d81234134ab219c083e1cf16ced2ee4bc1e093
Rebecca Robison <rebeccahrobison@outlook.com>
2023-12-08 10:03:34 -0600

    Added PostMeme functionality

--- Program.cs
+++ Program.cs
@@ -106,14 +106,15 @@ List<User> users = new List<User>()
 {
     new User { Id = 1, UserName = "josh", Password = "cats" },
     new User { Id = 2, UserName = "ryan", Password = "cats" },
-    new User { Id = 3, UserName = "rebeca", Password = "cats" },
+    new User { Id = 3, UserName = "rebecca", Password = "cats" },
     new User { Id = 4, UserName = "luc", Password = "cats" }
 };
 
 Console.Clear();
 
 bool UserLoggedIn = false;
-User user = null;
+// User user = null;
+User user = users[0];
 
 string loginScreen = @"Welcome to PawsPurrfect
 
@@ -175,7 +176,7 @@ void MainMenu()
 Menu Navigation:
 0. Exit
 1. View All Memes
-2. Something Else
+2. Post a New Meme
 3. Something Else
 ");
 
@@ -199,8 +200,7 @@ Please press any key to close the application");
             break;
         case "2":
             Console.Clear();
-            Console.WriteLine("This isn't implemented yet. Press any key to continue...");
-            Console.ReadKey();
+            PostMeme();
             break;
         case "3":
             Console.Clear();
@@ -229,3 +229,82 @@ void ViewAllMemes()
 Please press any key to continue...");
     Console.ReadKey();
 };
+
+void PostMeme()
+{
+    //Get Title, Image, and Description from logged in user
+    Console.WriteLine("Please enter the details of the cat meme to be posted:");
+    // Get Title
+    Console.WriteLine("Enter the title of your meme:");
+    string titleToPost = "";
+    while (string.IsNullOrEmpty(titleToPost) || titleToPost.Length > 100)
+    {
+        try
+        {
+            titleToPost = Console.ReadLine();
+            if (titleToPost.Length > 100)
+            {
+                throw new TooLongException("Title is too long. Reenter the title of your meme:");
+            }
+        }
+        catch (TooLongException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    // Get Image
+    Console.WriteLine("Enter the ASCII Image:");
+    string imageToPost = "";
+    while(string.IsNullOrEmpty(imageToPost))
+    {
+        try
+        {
+            imageToPost = Console.ReadLine();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+    }
+
+    // Get Description
+    Console.WriteLine("Enter the Meme's description:");
+    string descriptionToPost = "";
+    while(string.IsNullOrEmpty(descriptionToPost) || descriptionToPost.Length > 500)
+    {
+        try
+        {
+            descriptionToPost = Console.ReadLine();
+            if (descriptionToPost.Length > 500)
+            {
+                throw new TooLongException("Description is too long. Reenter the Meme's description:");
+            }
+        }
+        catch (TooLongException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    Meme memeToPost = new Meme
+    {
+        UserId = user.Id,
+        Title = titleToPost,
+        Image = imageToPost,
+        Description = descriptionToPost
+    };
+
+    memes.Add(memeToPost);
+    Console.WriteLine("Your cat meme has been added!");
+}
+
+
+
+public class TooLongException : Exception
+{
+  public TooLongException(string message) : base(message)
+  {
+
+  }
+}
